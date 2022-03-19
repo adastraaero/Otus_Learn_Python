252231b7671b60b8a10888ded265a1d26798805b
adastraaero <45203199+adastraaero@users.noreply.github.com>
2022-03-19 22:58:34 +0300

    finished homework_1 try_3

--- homework_01/main.py
+++ homework_01/main.py
@@ -2,24 +2,38 @@
 Домашнее задание №1
 Функции и структуры данных
 """
+from sympy import *
 
-
-def power_numbers():
+def power_numbers(*numbers, power=2):
     """
     функция, которая принимает N целых чисел,
     и возвращает список квадратов этих чисел
     >>> power_numbers(1, 2, 5, 7)
     <<< [1, 4, 25, 49]
     """
-
+    result = []
+    for number in numbers:
+        result.append(number ** power)
+    print(result)
 
 # filter types
 ODD = "odd"
 EVEN = "even"
 PRIME = "prime"
 
+def ODD(n):
+    return n % 2 != 0
 
-def filter_numbers():
+def EVEN(n):
+    return n % 2 == 0
+
+def PRIME(n):
+    return isprime(n)
+
+def filter_numbers(check, spisok):
+    print(list(filter(spisok, check)))
+
+
     """
     функция, которая на вход принимает список из целых чисел,
     и возвращает только чётные/нечётные/простые числа
